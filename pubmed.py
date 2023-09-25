979a4aa3753ae2295ddf6db145b9e8b77d637ad2
mkallipo <95910739+mkallipo@users.noreply.github.com>
2023-09-25 14:06:09 +0200

    Update pubmed.py

--- pubmed.py
+++ pubmed.py
@@ -80,14 +80,14 @@ def create_df(articleList):
     affList = []
 
     for i in range(len(df)):
-    affList_i = []
-    
-    try:
-        fi = final[i]['PubmedArticle']['MedlineCitation']['Article']['AuthorList']['Author']
-        # Continue processing fi here
-    except KeyError as e:
-        # Handle the KeyError exception here
-        print(f"KeyError: {e} occurred for index {i}")
+        affList_i = []
+        
+        try:
+            fi = final[i]['PubmedArticle']['MedlineCitation']['Article']['AuthorList']['Author']
+            # Continue processing fi here
+        except KeyError as e:
+            # Handle the KeyError exception here
+            print(f"KeyError: {e} occurred for index {i}")
 
 
         if type(fi) == dict:
