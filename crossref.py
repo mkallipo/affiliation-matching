6b37783543b25af4ced9a34489196ca80fbe71b6
mkallipo <95910739+mkallipo@users.noreply.github.com>
2024-09-25 16:51:00 +0200

    Provenance field

--- crossref.py
+++ crossref.py
@@ -261,13 +261,13 @@ def do(name, crossref_df):
                 new_x = []
                 for y in x:
                     if  dix_status[y['RORid']][0] == 'active':
-                        new_x.append({'PID':'ROR','Value':y['RORid'], 'Confidence': y['Confidence'], 'Status':'active'})
+                        new_x.append({'Provenance':'AffRo', 'PID':'ROR','Value':y['RORid'], 'Confidence': y['Confidence'], 'Status':'active'})
                 else:
                     if dix_status[y['RORid']][1] == '':
-                        new_x.append({'PID':'ROR','Value':y['RORid'], 'Confidence': y['Confidence'], 'Status':dix_status[y['RORid']][0]})
+                        new_x.append({'Provenance':'AffRo','PID':'ROR','Value':y['RORid'], 'Confidence': y['Confidence'], 'Status':dix_status[y['RORid']][0]})
                     else:
-                        new_x.append({'PID':'ROR','Value':y['RORid'], 'Confidence': y['Confidence'], 'Status':dix_status[y['RORid']][0]})
-                        new_x.append({'PID':'ROR','Value':dix_status[y['RORid']][1], 'Confidence': y['Confidence'], 'Status':'active'})
+                        new_x.append({'Provenance':'AffRo','PID':'ROR','Value':y['RORid'], 'Confidence': y['Confidence'], 'Status':dix_status[y['RORid']][0]})
+                        new_x.append({'Provenance':'AffRo','PID':'ROR','Value':dix_status[y['RORid']][1], 'Confidence': y['Confidence'], 'Status':'active'})
                 new_matching.append(new_x)
                     
 
